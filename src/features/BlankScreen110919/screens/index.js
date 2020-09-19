1062df725a0d9c088fef72e8976c98aa985338f4
Crowdbotics <team@crowdbotics.com>
2020-09-19 12:29:43 +0000

    Update components

--- src/features/BlankScreen110919/screens/index.js
+++ src/features/BlankScreen110919/screens/index.js
@@ -24,7 +24,6 @@ export class __Blank extends React.Component {
     <View style={this.props.themedStyle.View_1}>
       <View style={this.props.themedStyle.View_2}>
         <View style={this.props.themedStyle.View_3} />
-        <View style={this.props.themedStyle.View_4} />
         <View style={this.props.themedStyle.View_5} />
         <View style={this.props.themedStyle.View_6} />
       </View>
@@ -98,38 +97,6 @@ _Blank = withStyles(__Blank, theme => ({
     backgroundSize: "auto"
   },
   View_3: {
-    width: "50%",
-    height: "100%",
-    marginLeft: 0,
-    marginRight: 0,
-    marginTop: 0,
-    marginBottom: 0,
-    paddingLeft: 0,
-    paddingRight: 0,
-    paddingTop: 0,
-    paddingBottom: 0,
-    overflow: "visible",
-    fontSize: 12,
-    color: "#000000",
-    backgroundColor: "#ffffff",
-    fontStyle: "normal",
-    fontWeight: "normal",
-    flexDirection: "column",
-    flexWrap: "wrap",
-    justifyContent: "flex-start",
-    alignItems: "stretch",
-    alignContent: "stretch",
-    borderColor: "#000000",
-    borderStyle: "solid",
-    borderWidth: 0,
-    borderLeftWidth: 0,
-    borderRightWidth: 0,
-    borderTopWidth: 0,
-    borderBottomWidth: 0,
-    borderRadius: 0,
-    backgroundSize: "auto"
-  },
-  View_4: {
     width: "50%",
     height: "100%",
     marginLeft: 0,
